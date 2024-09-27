db3e0e535f5a4d1030bb7c3022216f6bc586faae
Ld28Fax <fandresenarasolomaminirina@gmail.com>
2024-09-27 10:09:45 +0300

    update
    verification d'erreur

--- project_JSAN/app/Http/Controllers/Auth/RegisteredUserController.php
+++ project_JSAN/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
+use Exception;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -30,37 +31,43 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        $request->validate([
-            'immatriculation' => ['required', 'string', 'max:255'],
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'Cour_appel' => ['required', 'string','max:255'],
-            'TPI' => ['required', 'string','max:255'],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'usertype' => ['required']
-        ], [
-            'immatriculation' => 'Le champ Immatriculation est requis',
-            'name' => 'Le champ Nom est requis, doit être une chaîne de caractères et ne doit pas dépasser 255 caractères.',
-            'email' => 'Le champ email est requis, doit être une chaîne de caractères, en minuscules.',
-            'Cour_appel' => 'Le champ Cour d\'appel est requis, doit être une chaîne de caractères, ne peut pas dépasser 255 caractères',
-            'TPI' => 'Le champ TPI est requis, doit être une chaîne de caractères et ne doit pas dépasser 255 caractères.',
-            'password' => 'Le champ mot de passe est requis, mot de passe ne correspond pas, ne respecte pas les critères de sécurité.'
-        ]);
+        try{
+            $request->validate([
+                'immatricule' => ['required', 'string', 'max:255'],
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+                'Cour_appel' => ['required', 'string','max:255'],
+                'TPI' => ['required', 'string','max:255'],
+                'password' => ['required', 'confirmed', Rules\Password::defaults()],
+                'usertype' => ['required']
+            ], [
+                'immatricule' => 'Le champ Immatriculation est requis',
+                'name' => 'Le champ Nom est requis, doit être une chaîne de caractères et ne doit pas dépasser 255 caractères.',
+                'email' => 'Le champ email est requis, doit être une chaîne de caractères, en minuscules.',
+                'Cour_appel' => 'Le champ Cour d\'appel est requis, doit être une chaîne de caractères, ne peut pas dépasser 255 caractères',
+                'TPI' => 'Le champ TPI est requis, doit être une chaîne de caractères et ne doit pas dépasser 255 caractères.',
+                'password' => 'Le champ mot de passe est requis, mot de passe ne correspond pas, ne respecte pas les critères de sécurité.'
+            ]);
+    
+            $user = User::create([
+                'immatricule' => $request->immatricule,
+                'name' => $request->name,
+                'email' => $request->email,
+                'Cour_appel' => $request->Cour_appel,
+                'TPI' => $request->TPI,
+                'password' => Hash::make($request->password),
+                'usertype' => 0,
+            ]);
+    
+            event(new Registered($user));
+    
+            Auth::login($user);
+    
+            return redirect(RouteServiceProvider::HOME);
 
-        $user = User::create([
-            'immatriculation' => $request->immatriculation,
-            'name' => $request->name,
-            'email' => $request->email,
-            'Cour_appel' => $request->Cour_appel,
-            'TPI' => $request->TPI,
-            'password' => Hash::make($request->password),
-            'usertype' => 0,
-        ]);
-
-        event(new Registered($user));
-
-        Auth::login($user);
-
-        return redirect(RouteServiceProvider::HOME);
+        } catch(Exception $e){
+            // throw new Exception($e->getMessage());
+            return redirect()->back()->withErrors(['error' => 'Une erreur s\'est produite : ' . $e->getMessage()]);
+        }
     }
 }
