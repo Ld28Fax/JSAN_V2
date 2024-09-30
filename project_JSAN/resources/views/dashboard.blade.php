8a20e3b39de5ac59075d0ceab424e104303f33c7
Ld28Fax <fandresenarasolomaminirina@gmail.com>
2024-09-30 08:52:48 +0300

    commit

--- project_JSAN/resources/views/dashboard.blade.php
+++ project_JSAN/resources/views/dashboard.blade.php
@@ -101,7 +101,7 @@
           <div>
             <div class="card-body">
               <div class="text-center">
-                <a href="{{ route('profile.edit') }}"><i class="fas fa-user profile-user-img img-fluid img-circle" style='width:20%;height:50%  ' ></i></a>
+                <a href="{{ route('profile.edit') }}"><img src="extern/dist/img/user.png" alt="" class="brand-image img-circle elevation-3" style="opacity: .8; width:40%"></a>
               </div>
 
               <a href="{{ route('profile.edit') }}"><h3 class="profile-username text-center">{{ Auth::user()->name }}</h3></a>
